52451a989eb3ee7699dabf536bf4b0db660e31aa
Hassan Momin <53341268+b3ingHassan@users.noreply.github.com>
2023-09-14 23:17:48 +0530

    tablet portfolio section added

--- lib/portfolioSection/portfolio_tablet_section.dart
+++ lib/portfolioSection/portfolio_tablet_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_portfolio/utils/colors.dart';
 
 class PortfolioTabletSection extends StatefulWidget {
   const PortfolioTabletSection({super.key});
@@ -10,6 +11,126 @@ class PortfolioTabletSection extends StatefulWidget {
 class _PortfolioTabletSectionState extends State<PortfolioTabletSection> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double w = MediaQuery.of(context).size.width;
+
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: w * 0.02,
+        vertical: w * 0.1,
+      ),
+      color: AppColors.bgWhite1,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Portfolio",
+            style: TextStyle(
+              fontSize: w * 0.02,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          SizedBox(
+            height: w * 0.01,
+          ),
+          Text(
+            "Best Projects",
+            style: TextStyle(
+              fontSize: w * 0.03,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: w * 0.024,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              PortfolioCustomWidget(
+                w: w,
+                description: "Something",
+                title: "GharTak Food Delivery App",
+              ),
+               PortfolioCustomWidget(
+                w: w,
+                description: "Something",
+                title: "GharTak Food Delivery App",
+              ),
+
+            ],
+          ),SizedBox(
+            height: w*0.02,
+          ),  Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              PortfolioCustomWidget(
+                w: w,
+                description: "Something",
+                title: "GharTak Food Delivery App",
+              ),
+               PortfolioCustomWidget(
+                w: w,
+                description: "Something",
+                title: "GharTak Food Delivery App",
+              ),
+                
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PortfolioCustomWidget extends StatelessWidget {
+  const PortfolioCustomWidget({
+    super.key,
+    required this.w,
+    required this.title,
+    required this.description,
+  });
+
+  final double w;
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: w * 0.28,
+          width: w * 0.47,
+          color: AppColors.accent,
+        ),
+        SizedBox(
+          height: w * 0.01,
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: w * 0.02,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        SizedBox(
+          height: w * 0.01,
+        ),
+        SizedBox(
+          width: w * 0.47,
+          child: Text(
+            description,
+            style: TextStyle(
+              fontSize: w * 0.02,
+              height: 1.8,
+              fontWeight: FontWeight.normal,
+              color: AppColors.subtitleTxt2,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
