c2508f9cb892c1ffa744ebd9dbb42963e6d95053
Hassan Momin <53341268+b3ingHassan@users.noreply.github.com>
2023-09-14 23:47:45 +0530

    Footer tablet section added

--- lib/pages/footerSection/footer_tablet_section.dart
+++ lib/pages/footerSection/footer_tablet_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_portfolio/utils/colors.dart';
 
 class FooterTabletSection extends StatefulWidget {
   const FooterTabletSection({super.key});
@@ -10,6 +11,86 @@ class FooterTabletSection extends StatefulWidget {
 class _FooterTabletSectionState extends State<FooterTabletSection> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double w = MediaQuery.of(context).size.width;
+
+    return Container(
+      height: w*0.7,
+      width: w,
+      color: AppColors.bgColor2,
+      padding: EdgeInsets.symmetric(
+        horizontal: w * 0.02,
+        vertical: w * 0.1,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "GET IN TOUCH",
+            style: TextStyle(
+              fontSize: w * 0.02,
+              fontWeight: FontWeight.normal,
+              color: AppColors.bgWhite2,
+            ),
+          ),
+          SizedBox(
+            height: w * 0.01,
+          ),
+          Text(
+            "Let's Connect",
+            style: TextStyle(
+                fontSize: w * 0.03,
+                fontWeight: FontWeight.bold,
+                color: AppColors.accent),
+          ),
+          SizedBox(
+            height: w * 0.024,
+          ),
+          Text(
+            "Have a project or opportunity in mind? Let's have a\nnice chat over it. Contact me here or email me at",
+            style: TextStyle(
+              fontSize: w * 0.02,
+              fontWeight: FontWeight.normal,
+              color: AppColors.bgWhite1,
+            ),
+          ),
+          SizedBox(
+            height: w * 0.04,
+          ),
+          Container(
+            width: w * 0.34,
+            height: w * 0.058,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+             
+              color: AppColors.primary,
+            ),
+            child: Center(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.download,
+                    color: AppColors.bgWhite1,
+                  ),
+                  SizedBox(
+                    width: w * 0.008,
+                  ),
+                  Text(
+                    "hassanwm99@gmail.com",
+                    style: TextStyle(
+                      fontSize: w * 0.02,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.bgWhite1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
