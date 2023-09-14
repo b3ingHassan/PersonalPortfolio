fa54d02efe9359a750705b3a37fd0d857fe4632d
Hassan Momin <53341268+b3ingHassan@users.noreply.github.com>
2023-09-14 23:33:58 +0530

    added tablet about me section

--- lib/pages/aboutMeSection/about_me_tablet_section.dart
+++ lib/pages/aboutMeSection/about_me_tablet_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_portfolio/utils/colors.dart';
 
 class AboutMeTabletSection extends StatefulWidget {
   const AboutMeTabletSection({super.key});
@@ -10,6 +11,81 @@ class AboutMeTabletSection extends StatefulWidget {
 class _AboutMeTabletSectionState extends State<AboutMeTabletSection> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double w = MediaQuery.of(context).size.width;
+    return Container(
+      width: w,
+      padding: EdgeInsets.symmetric(
+        horizontal: w * 0.02,
+        vertical: w * 0.1,
+      ),
+      color: AppColors.bgWhite1,
+      child: Row(
+        children: [
+          SizedBox(
+            width: w * 0.46,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Hi there, I'm",
+                  style: TextStyle(
+                    fontSize: w * 0.02,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(
+                  height: w * 0.01,
+                ),
+                Text(
+                  "Hassan Momin",
+                  style: TextStyle(
+                    fontSize: w * 0.03,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: w * 0.024,
+                ),
+                Text(
+                  "Planning in the right direction after the identification of the problem space and stiching the right solution according to that is very cruicial.",
+                  style: TextStyle(
+                    fontSize: w * 0.02,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(
+                  height: w * 0.01,
+                ),
+                Text(
+                  "Planning in the right direction after the identification of the problem space and stiching the right solution according to that is very cruicial.",
+                  style: TextStyle(
+                    fontSize: w * 0.02,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(
+                  height: w * 0.01,
+                ),
+                Text(
+                  "Planning in the right direction after the identification of the problem space and stiching the right solution according to that is very cruicial.",
+                  style: TextStyle(
+                    fontSize: w * 0.02,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Image.asset(
+              "assets/images/aboutme.png",
+              height: w * 0.4,
+              fit: BoxFit.contain,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
